90fcffb1fc4427dae71fae89de616783a29f5e84
Femi Adekunle <gideonf.adekunle@gmail.com>
2021-12-07 09:40:54 +0000

    day 07

--- day_07/day_07.py
+++ day_07/day_07.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import pandas as pd
 
 
 if __name__ == '__main__':
@@ -10,21 +9,17 @@ if __name__ == '__main__':
 
     # part one
     fuel_cost = 9999999999999
-    a = pd.Series(input_list)
     for v in possible_pos:
-        b = a.copy()
-        b = abs(b - v)
-        if (cost := b.sum()) < fuel_cost:
+        b = sum([abs(x-v) for x in input_list])
+        if (cost := b) < fuel_cost:
             fuel_cost = cost
     print(fuel_cost)
 
     # part two
     fuel_cost = 9999999999999
-    a = pd.Series(input_list)
     for v in possible_pos:
-        b = a.copy()
-        b = abs(b - v)
-        b = b.apply(lambda x: sum([y for y in range(1, x+1)]))
-        if (cost := b.sum()) < fuel_cost:
+        b = [abs(x - v) for x in input_list]
+        c = sum([sum([y for y in range(1, x+1)]) for x in b])
+        if (cost := c) < fuel_cost:
             fuel_cost = cost
     print(fuel_cost)
